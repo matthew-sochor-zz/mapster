4d21be4dad2d225a914fb08bc556aa6d63753631
matthew-sochor <matthew.sochor@gmail.com>
2015-03-06 15:25:24 -0500

    Data Incubator Keystone Project
    First commit of DI Keystone Project

---
+++ mercator.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+import numpy as np
+from math import log, exp, tan, atan, pi, ceil, radians, sin, cos, atan2, sqrt
+
+EARTH_RADIUS = 6378137
+EQUATOR_CIRCUMFERENCE = 2 * pi * EARTH_RADIUS
+ORIGIN_SHIFT = EQUATOR_CIRCUMFERENCE / 2.0
+google_res = 512
+
+def great_circle_distance(latlong_a, latlong_b):
+
+    lat1, lon1 = latlong_a
+    lat2, lon2 = latlong_b
+     
+    dLat = radians(lat2 - lat1)
+    dLon = radians(lon2 - lon1)
+    a = (sin(dLat / 2) * sin(dLat / 2) +
+    cos(radians(lat1)) * cos(radians(lat2)) *
+    sin(dLon / 2) * sin(dLon / 2))
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+    d = EARTH_RADIUS * c
+    return d 
+
+def latlontopixels(lat, lon, zoom):
+    mx = (lon * ORIGIN_SHIFT) / 180.0
+    my = log(tan((90.0 + lat) * pi/360.0))/(pi/180.0)
+    my = (my * ORIGIN_SHIFT) /180.0
+    res = EQUATOR_CIRCUMFERENCE/google_res*2/(2.0**zoom)
+    px = (mx + ORIGIN_SHIFT)/res
+    py = (my + ORIGIN_SHIFT)/res
+    return px, py
+
+def pixelstolatlon(px, py, zoom):
+    res = EQUATOR_CIRCUMFERENCE/google_res*2/(2**zoom)
+    mx = px * res - ORIGIN_SHIFT
+    my = py * res - ORIGIN_SHIFT
+    lat = (my / ORIGIN_SHIFT) * 180.0
+    lat = 180.0 / pi * (2.0*atan(exp(lat*pi/180.0)) - pi/2.0)
+    lon = (mx / ORIGIN_SHIFT) * 180.0
+    return lat, lon
+
+def pixelstolat(py, zoom):
+    res = EQUATOR_CIRCUMFERENCE/google_res*2/(2**zoom)
+    my = py * res - ORIGIN_SHIFT
+    lat = (my / ORIGIN_SHIFT) * 180.0
+    lat = 180 / pi * (2*atan(exp(lat*pi/180.0)) - pi/2.0)
+    return lat
+
+def pixelstolon(px, zoom):
+    res = EQUATOR_CIRCUMFERENCE/google_res*2/(2**zoom)
+    mx = px * res - ORIGIN_SHIFT
+    lon = (mx / ORIGIN_SHIFT) * 180.0
+    return lon
+
---
+++ yelp.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+import argparse
+import json
+import pprint
+import sys
+import urllib
+import urllib2
+
+import oauth2
+
+
+API_HOST = 'api.yelp.com'
+DEFAULT_TERM = 'dinner'
+DEFAULT_LOCATION = 'San Francisco, CA'
+SEARCH_LIMIT = 3
+SEARCH_PATH = '/v2/search/'
+BUSINESS_PATH = '/v2/business/'
+
+# OAuth credential placeholders that must be filled in by users.
+CONSUMER_KEY = 'rmQ0uSnjOGyg1LwVHgxj0Q'
+CONSUMER_SECRET = 'QDLpYmmBXOB4Iqhu54BbW1YwfUY'
+TOKEN = 'PdH4mzjxfQbizTh1gB43qpjyiHgDKk8d'
+TOKEN_SECRET = 'ECeiAatgdAtN_WFb-4kxIPXjjf0'
+
+def yelp_request(host, path, url_params=None):
+    """Prepares OAuth authentication and sends the request to the API.
+
+    Args:
+        host (str): The domain host of the API.
+        path (str): The path of the API after the domain.
+        url_params (dict): An optional set of query parameters in the request.
+
+    Returns:
+        dict: The JSON response from the request.
+
+    Raises:
+        urllib2.HTTPError: An error occurs from the HTTP request.
+    """
+    url_params = url_params or {}
+    url = 'http://{0}{1}?'.format(host, path)
+    
+    consumer = oauth2.Consumer(CONSUMER_KEY, CONSUMER_SECRET)
+    oauth_request = oauth2.Request(method="GET", url=url, parameters=url_params)
+
+    oauth_request.update(
+        {
+            'oauth_nonce': oauth2.generate_nonce(),
+            'oauth_timestamp': oauth2.generate_timestamp(),
+            'oauth_token': TOKEN,
+            'oauth_consumer_key': CONSUMER_KEY
+        }
+    )
+    token = oauth2.Token(TOKEN, TOKEN_SECRET)
+    oauth_request.sign_request(oauth2.SignatureMethod_HMAC_SHA1(), consumer, token)
+    signed_url = oauth_request.to_url()
+    
+    #print 'Querying {0} ...'.format(url)
+
+    conn = urllib2.urlopen(signed_url, None)
+    try:
+        response = json.loads(conn.read())
+    finally:
+        conn.close()
+
+    return response
+
+def yelp_search(term, location):
+    """Query the Search API by a search term and location.
+
+    Args:
+        term (str): The search term passed to the API.
+        location (str): The search location passed to the API.
+
+    Returns:
+        dict: The JSON response from the request.
+    """
+    
+    url_params = {
+        'term': term.replace(' ', '+'),
+        'location': location.replace(' ', '+'),
+        'limit': SEARCH_LIMIT
+    }
+    
+    return yelp_request(API_HOST, SEARCH_PATH, url_params=url_params)
+
+def yelp_get_business(business_id):
+    """Query the Business API by a business ID.
+
+    Args:
+        business_id (str): The ID of the business to query.
+
+    Returns:
+        dict: The JSON response from the request.
+    """
+    business_path = BUSINESS_PATH + business_id
+
+    return yelp_request(API_HOST, business_path)
+
